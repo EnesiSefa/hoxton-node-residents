2f5a1acaceb9b2cbcc95bc8f543c0d18d612f5ba
enesi sefa <enesisefa380@gmail.com>
2022-09-02 16:48:07 +0200

    only patch remaining

--- data.ts
+++ data.ts
@@ -1,13 +1,30 @@
-export const residents = [
-  { id: 1, name: "Paul", age: 29, gender: "male" },
-  { id: 2, name: "Scarlet", age: 30, gender: "female" },
-  { id: 3, name: "Monica", age: 28, gender: "female" },
-  { id: 4, name: "joey", age: 27, gender: "male" },
+type House = {
+  id: number;
+  address: string;
+  type: string;
+  
+  // Resident?: Resident[];
+};
+type Resident = {
+  id: number;
+  name: string;
+  age: number;
+  gender: string;
+  houseId: number;
+  // house?: House[];
+};
+
+export const residents: Resident[] = [
+  { id: 1, name: "Paul", age: 29, gender: "male" , houseId:1},
+  { id: 2, name: "Scarlet", age: 30, gender: "female" ,houseId:2},
+  { id: 3, name: "Monica", age: 28, gender: "female" ,houseId:3},
+  { id: 4, name: "joey", age: 27, gender: "male" ,houseId:4},
 ];
 
-export const houses = [
-  { id: 1, address: "1428 Elm Street,", type: "house", residentId: 4 },
-  { id: 2, address: "123 farm Street", type: "farm" ,residentId:3},
-  { id: 3, address: "777 New York Street", type: "flat" ,residentId:4},
-  { id: 4, address: "676 Manhattan Street", type: "flat" ,residentId:2},
+export const houses: House[] = [
+  { id: 1, address: "1428 Elm Street,", type: "house",  },
+  { id: 2, address: "123 farm Street", type: "farm",  },
+  { id: 3, address: "234 Texas Farm Street ", type: "farm", },
+  { id: 4, address: "676 Manhattan Street", type: "flat",  },
+  { id: 5, address: "890 Washington Street", type: "house",  },
 ];
--- server.ts
+++ server.ts
@@ -1,19 +1,129 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
+
 import { residents, houses } from "./data";
 
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send(`write this link http://localhost:${port}/residents or /houses`);
-  });
+  res.send(`write this link http://localhost:${port}/residents or /houses`);
+});
 app.get("/residents", (req, res) => {
-  res.send(residents);
+  let residentsToSend = residents.map((resident) => {
+    let house = houses.find((house) => house.id === resident.houseId);
+    return { ...resident, house };
+  });
+
+  res.send(residentsToSend);
+});
+app.get("/residents/:id", (req, res) => {
+  let searchedId = Number(req.params.id);
+  let match = residents.filter((resident) => resident.id === searchedId);
+  res.send(match);
 });
 
 app.get("/houses", (req, res) => {
-  res.send(houses);
+  let housesToSend = houses.map((house) => {
+    let resident = residents.find((resident) => resident.id === house.id);
+    return { ...house, resident };
+  });
+
+  res.send(housesToSend);
+});
+app.get("/houses/:id", (req, res) => {
+  let searchedId = Number(req.params.id);
+  let match = houses.filter((house) => house.id === searchedId);
+
+  res.send(match);
+});
+
+app.post("/residents", (req, res) => {
+  let errors: String[] = [];
+  console.log(req.body);
+
+  if (typeof req.body.name !== "string") {
+    errors.push("name not provided or not a string.");
+  }
+
+  if (typeof req.body.age !== "number") {
+    errors.push("age not provided or not a number.");
+  }
+
+  if (typeof req.body.gender !== "string") {
+    errors.push("gender not provided or not a string.");
+  }
+  if (typeof req.body.houseId !== "number") {
+    errors.push("houseId not provided or not a number.");
+  }
+
+  // if there are no errors, create the item
+  if (errors.length === 0) {
+    const newItem = {
+      id: residents[residents.length - 1].id + 1,
+      name: req.body.name,
+      age: req.body.age,
+      gender: req.body.gender,
+      houseId: req.body.houseId,
+    };
+    residents.push(newItem);
+    res.send(newItem);
+  } else {
+    // if there are any errors...
+    res.status(400).send({ errors: "please write the correct keys" });
+  }
+});
+
+app.post("/houses", (req, res) => {
+  let errors: String[] = [];
+  console.log(req.body);
+
+  if (typeof req.body.address !== "string") {
+    errors.push("address not provided or not a string.");
+  }
+
+  if (typeof req.body.type !== "string") {
+    errors.push("type not provided or not a string.");
+  }
+
+  // if there are no errors, create the house
+  if (errors.length === 0) {
+    const newItem = {
+      id: residents[residents.length - 1].id + 1,
+      address: req.body.address,
+      type: req.body.type,
+    };
+    houses.push(newItem);
+    res.send(newItem);
+  } else {
+    // if there are any errors...
+    res.status(400).send({ errors: "please write the correct keys" });
+  }
+});
+
+app.delete("/houses/:id", (req, res) => {
+  const foundId = Number(req.params.id);
+
+  if (foundId > -1) {
+    // -1 means the object that doesnt exist
+    let deletedHouse = houses.splice(foundId, 1)[0];
+    // from the array i get the first element [0] because splice returns an array with an object inside
+    res.send({ message: "quote deleted successfully", deletedHouse });
+  }
+});
+
+app.delete("/residents/:id", (req, res) => {
+  const foundId = Number(req.params.id);
+  const idToDelete = houses.findIndex((resident) => resident.id === foundId);
+
+  if (idToDelete > -1) {
+    // -1 means the object that doesnt exist
+    let deletedResident = residents.splice(idToDelete, 1)[0];
+    // from the array i get the first element [0] because splice returns an array with an object inside
+    res.send({ message: "quote deleted successfully", deletedResident });
+  }
 });
 
 app.listen(port, () => {
